a0028be26283dece04ca586a6ba2259ff363c4e1
Jonah Hirsch <jonah@giftcardzen.com>
2014-10-31 08:42:11 -0700

    Updated error message

--- lib/trustev/error.rb
+++ lib/trustev/error.rb
@@ -2,28 +2,17 @@ module Trustev
   class Error < StandardError
     attr_reader :message
     attr_reader :http_status
-    attr_reader :http_body
-    attr_reader :json_body
-    attr_reader :param
 
-    def initialize(message=nil, http_status=nil, http_body=nil, json_body=nil)
-      @message = message
+    def initialize(message=nil, http_status=nil, trustev_message=nil)
+      @message = "#{message} #{trustev_message}"
       @http_status = http_status
-      @http_body = http_body
-      @json_body = json_body
-
-      begin
-        @param = @json_body[:error][:param]
-      rescue
-        @param = nil
-      end
 
       super(message)
     end
 
     def to_s
       status_string = @http_status.nil? ? '' : "(Status #{@http_status}) "
-      "#{status_string}#{@message} #{@http_body}"
+      "#{status_string}#{@message}"
     end
   end
 end
